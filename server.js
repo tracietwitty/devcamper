0a7c25bb2c5bb6128341be9b9ac6f535100c54a9
Tracie T <traciecodes@gmail.com>
2020-01-17 04:44:26 -0600

    Set up initial routes/endpoints

--- server.js
+++ server.js
@@ -6,6 +6,39 @@ dotenv.config({ path: './config/config.env' });
 
 //init app variable
 const app = express();
+
+// GET all bootcamps from db
+app.get('/api/v1/bootcamps', (req, res) => {
+	res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
+// GET specific bootcamp from db
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+	res
+		.status(200)
+		.json({ success: true, msg: `Get specific bootcamp: ${req.params.id}` });
+});
+
+// POST new bootcamps to db
+app.post('/api/v1/bootcamps', (req, res) => {
+	res.status(200).json({ success: true, msg: 'Add new bootcamp to db' });
+});
+
+// PUT update specific bootcamp in db
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+	res.status(200).json({
+		success: true,
+		msg: `Update specific bootcamp: ${req.params.id} `
+	});
+});
+
+// DELETE specific bootcamp from db
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+	res
+		.status(200)
+		.json({ success: true, msg: `Delete specific bootcamp: ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(
 	PORT,
