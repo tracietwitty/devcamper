04fe42fde61a755704696764498a2c8322d2fd13
Tracie T <traciecodes@gmail.com>
2020-01-18 10:39:18 -0600

    Added morgan to dependencies and included it in server.js

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
-// Route files
+// route files:
 const bootcamps = require('./routes/bootcamps');
 
-// Load env vars
+// load env vars:
 dotenv.config({ path: './config/config.env' });
 
-//init app variable
+//init app variable:
 const app = express();
 
-// mount routers
+// dev logging middleware:
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
+
+// mount routers:
 app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5000;
 app.listen(
